8f1d342ceb5b33a46c792cb787e659820716adc2
ovechko.056@gmail.com <ovechko.056@gmail.com>
2017-07-09 15:25:31 +0300

    new build script

---
+++ android.sh
@@ -0,0 +1,14 @@
+echo "Removing the app..."
+adb shell pm clear art.neural.ovechko.neuralart_android
+
+echo "Building .so lib..."
+
+sh neuralart/build.sh
+
+echo "Gradle..."
+
+#./gradlew installDebug
+
+#echo "Launch the app..."
+
+#adb shell monkey -p art.neural.ovechko.neuralart_android -c android.intent.category.LAUNCHER 1
